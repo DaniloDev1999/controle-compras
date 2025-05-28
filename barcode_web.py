6619c4ee55f73a3bb951d7f343c07e67a9538cff
Danilo da silva araujo <97401474+DaniloDev1999@users.noreply.github.com>
2025-05-28 16:11:22 -0300

    Add files via upload

--- barcode_web.py
+++ barcode_web.py
@@ -2,37 +2,44 @@ import streamlit.components.v1 as components
 import streamlit as st
 
 def escanear_codigo_web():
-    # Contêiner para o valor lido
-    codigo_lido = st.empty()
+    st.markdown("### 📸 Posicione o código de barras na frente da câmera")
 
-    # Código HTML + JS para o leitor
     components.html(
         """
-        <div id="reader" width="300px"></div>
-        <p id="result"></p>
+        <div id="reader" width="100%"></div>
+        <p id="result" style="font-weight: bold;"></p>
         <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
         <script>
-        const html5QrCode = new Html5Qrcode("reader");
+            const html5QrCode = new Html5Qrcode("reader");
 
-        function onScanSuccess(decodedText, decodedResult) {
-            document.getElementById("result").innerText = decodedText;
-            const streamlitEvent = new Event("input", { bubbles: true });
-            const streamlitInput = window.parent.document.querySelector('iframe').contentWindow.document.querySelectorAll("input[type='text']")[0];
-            streamlitInput.value = decodedText;
-            streamlitInput.dispatchEvent(streamlitEvent);
-            html5QrCode.stop();
-        }
+            function onScanSuccess(decodedText, decodedResult) {
+                const streamlitInput = window.parent.document.querySelector('iframe').contentWindow.document.querySelectorAll("input[type='text']")[0];
+                streamlitInput.value = decodedText;
+                streamlitInput.dispatchEvent(new Event("input", { bubbles: true }));
+                html5QrCode.stop();
+            }
 
-        html5QrCode.start(
-            { facingMode: "environment" },  // câmera traseira
-            {
-                fps: 10,
-                qrbox: 250
-            },
-            onScanSuccess
-        ).catch(err => {
-            document.getElementById("result").innerText = "Erro ao acessar a câmera: " + err;
-        });
+            Html5Qrcode.getCameras().then(devices => {
+                if (devices && devices.length) {
+                    html5QrCode.start(
+                        { facingMode: "environment" },
+                        {
+                            fps: 10,
+                            qrbox: 250,
+                            formatsToSupport: [
+                                Html5QrcodeSupportedFormats.CODE_128,
+                                Html5QrcodeSupportedFormats.EAN_13,
+                                Html5QrcodeSupportedFormats.EAN_8,
+                                Html5QrcodeSupportedFormats.UPC_A,
+                                Html5QrcodeSupportedFormats.UPC_E
+                            ]
+                        },
+                        onScanSuccess
+                    ).catch(err => {
+                        document.getElementById("result").innerText = "Erro ao iniciar a câmera: " + err;
+                    });
+                }
+            });
         </script>
         """,
         height=400
